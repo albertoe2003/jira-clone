c0c34d224b3d2231f4da11278b433d4e602b16a2
albertoe2003 <albertovoeffray@gmail.com>
2025-05-21 19:03:58 -0300

    update

--- src/features/test.tsx
+++
@@ -1,8 +0,0 @@
-export const TestComponent =()=>{
-        return (
-                <div className="border-border text-red-500 p-0.5 p-">
-                test
-                </div>
-        
-        )
-}
