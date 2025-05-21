0a90cf7c5509f0aa4d10357fbdbdfc20ed86dd5f
albertoe2003 <albertovoeffray@gmail.com>
2025-05-21 11:59:47 -0300

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { TestComponent } from "@/features/test";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <TestComponent/>
+      <Button variant="primary">primary</Button>
+      <Button variant="primary">primary</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="teritary">teritary</Button>
     </div>
   );
 }
